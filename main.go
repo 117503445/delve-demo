679e86d0e264bb6700966ab44c8d75db3cb7372f
117503445 <t117503445@gmail.com>
2024-05-27 07:01:40 +0000

    feat: fiber
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v3"
+	"log"
+)
 
 func main() {
 	a := 1
 	fmt.Println("1")
 	fmt.Println(a)
+
+	// Initialize a new Fiber app
+	app := fiber.New()
+
+	// Define a route for the GET method on the root path '/'
+	app.Get("/", func(c fiber.Ctx) error {
+		b := 2
+		fmt.Println(b)
+		// Send a string response to the client
+		return c.SendString("Hello, World 👋!")
+	})
+
+	// Start the server on port 3000
+	log.Fatal(app.Listen(":3000"))
 }
--- note.md
+++ note.md
@@ -1,2 +1,2 @@
 docker build -t delve-demo .
-docker run -p 2345:2345 --rm --name delve-demo delve-demo
+docker run -p 2345:2345 -p 3000:3000 --rm --name delve-demo delve-demo
